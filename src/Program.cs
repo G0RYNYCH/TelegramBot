1b2d1908ac8aecf2c518eac003b5aeeb62e269ff
G0RYNYCH <92940275+GORYNTCHdotNET@users.noreply.github.com>
2022-08-10 17:10:45 +0300

    #1: close test run

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var botClient = new TelegramBotClient("5509847805:AAENAqMzNRfFoG30Yo3iwVbnUTDyXyB4xrA");
+
+var me = await botClient.GetMeAsync();
+Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
