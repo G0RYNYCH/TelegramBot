006f8d6222e32724d4a0fbbd929ca24d3e6c7d2c
G0RYNYCH <92940275+GORYNTCHdotNET@users.noreply.github.com>
2022-08-11 02:29:59 +0300

    close issue #3: First Chat Bot

--- src/GlobalUsings.cs
+++ src/GlobalUsings.cs
@@ -1,1 +1,5 @@
 ﻿global using Telegram.Bot;
+global using Telegram.Bot.Exceptions;
+global using Telegram.Bot.Polling;
+global using Telegram.Bot.Types;
+global using Telegram.Bot.Types.Enums;
--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,50 @@
 ﻿var botClient = new TelegramBotClient("5509847805:AAENAqMzNRfFoG30Yo3iwVbnUTDyXyB4xrA");
 
+using var cts = new CancellationTokenSource();
+
+var receiverOptions = new ReceiverOptions
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+);
+
 var me = await botClient.GetMeAsync();
-Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Message is not { } message)
+        return;
+    if (message.Text is not { } messageText)
+        return;
+
+    var chatId = message.Chat.Id;
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+    Message sentMessage = await botClient.SendTextMessageAsync(
+        chatId: chatId,
+        text: "You said:\n" + messageText,
+        cancellationToken: cancellationToken);
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var errorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.Message
+    };
+
+    return Task.CompletedTask;
+}
